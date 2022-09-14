c86796bbe3616a8202fb222eece2698f057f9951
chegarty74 <chegarty74@yahoo.com>
2022-09-13 21:12:13 -0500

    missing middleware in server.js

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(passport.initialize());
 app.use(express.static(__dirname + "/build"));
 
 app.use("/api/users", userRoutes);
