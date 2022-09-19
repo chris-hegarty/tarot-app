bb49b4b53a03f6badbac06f834a8eb575879b86f
chegarty74 <chegarty74@yahoo.com>
2022-09-18 22:06:29 -0500

    Added scroll to cards functionality but it is taking two clicks?

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useCallback, useRef } from "react";
 import useAxios from "../hooks/useAxios";
 import { TarotCardContext } from "../context/TarotCardContext";
 import TarotCard from "./TarotCard";
@@ -9,23 +9,36 @@ function HomePage(props) {
 	const { data: cards, error } = useAxios(url);
 	const { revealCard, setRevealCard } = useContext(TarotCardContext);
 
+	let cardScroll = useRef(null);
+
+	const handleScroll = useCallback(() => {
+		cardScroll.current.scrollIntoView();
+	}, [show]);
+
 	return (
 		<main>
 			<section className="home-top flex">
 				<div className="half left basis-1/2 p-16">
 					<h2 className="">Getting Started</h2>
 					<p>
-						First step: Focus on a question, problem, decision or any uncertain area on your mind.  
+						First step: Focus on a question, problem, decision or any uncertain
+						area on your mind.
 					</p>
 					<p>
-						You are going to get a three-card, "linear" tarot card spread. Depending on your focus, the first, second and third cards can suggest perspectives in a number of ways, like:
+						You are going to get a three-card, "linear" tarot card spread.
+						Depending on your focus, the first, second and third cards can
+						suggest perspectives in a number of ways, like:
 					</p>
 					<ul>
 						<li>Past | Present | Future</li>
 						<li>A Situation | An Action | An Outcome</li>
 						<li>Option 1 | Option 2 | Advice/Direction</li>
 					</ul>
-					<p>Think about the question you are focused on, and what three "here to there" points the cards could help you think on.</p>
+					<p>
+						Think about the question you are focused on, and what three "here to
+						there" points the cards could help you think on. Then, scroll down
+						and click on "Get Cards".
+					</p>
 				</div>
 				<div className="half right basis-1/2">
 					<picture>
@@ -45,18 +58,23 @@ function HomePage(props) {
 							e.preventDefault();
 							setShow(true);
 							setUrl(`/random/?n=3`);
+							handleScroll();
 							// setMeaning()
 						}}
-						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+						className="text-lg uppercase bg-red-700 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
 					>
 						Get Cards
 					</button>
 				</div>
-				<div className={!show ? "cards-container hidden" : "cards-container"}>
-					<p>
-						Now, one by one, click to to reveal the card and its description.
-					</p>
-					<div className="flex justify-evenly">
+				<div
+					className={
+						!show ? "cards-container hidden" : "cards-container px-10 py-6 mb-8"
+					}
+				>
+					<h2 ref={cardScroll} className="text-center">
+						Now, click on each card to reveal it.
+					</h2>
+					<div className="flex justify-evenly px-10">
 						{error && error}
 						{cards &&
 							cards.length > 0 &&
